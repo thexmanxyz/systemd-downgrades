3b84b7503a375004a3bd1e6c1d63e7dada8cf99e
thex <andreas.kar@gmx.at>
2019-01-15 17:10:21 +0100

    modified sh-bang and if syntax

--- systemd-reset-21_10.sh
+++ systemd-reset-21_10.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 mkdir systemd-tmp
 wget http://launchpadlibrarian.net/399247908/libpam-systemd_229-4ubuntu21.10_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/399247910/libsystemd0_229-4ubuntu21.10_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/399247918/systemd_229-4ubuntu21.10_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/399247917/systemd-sysv_229-4ubuntu21.10_armhf.deb -P systemd-tmp
 sudo dpkg -i systemd-tmp/libpam-systemd_229-4ubuntu21.10_armhf.deb systemd-tmp/libsystemd0_229-4ubuntu21.10_armhf.deb systemd-tmp/systemd_229-4ubuntu21.10_armhf.deb systemd-tmp/systemd-sysv_229-4ubuntu21.10_armhf.deb
 rm systemd-tmp/libpam-systemd_229-4ubuntu21.10_armhf.deb | rm systemd-tmp/libsystemd0_229-4ubuntu21.10_armhf.deb | rm systemd-tmp/systemd_229-4ubuntu21.10_armhf.deb | rm systemd-tmp/systemd-sysv_229-4ubuntu21.10_armhf.deb
 rmdir systemd-tmp
-if [ ${1:-0} == "1" ]
+if [ ${1:-0} = "1" ]
 then
 sudo apt-mark hold libsystemd0 libpam-systemd systemd systemd-sysv
 fi
--- systemd-reset-21_11.sh
+++ systemd-reset-21_11.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 mkdir systemd-tmp
 wget http://launchpadlibrarian.net/401375344/libpam-systemd_229-4ubuntu21.11_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/401375346/libsystemd0_229-4ubuntu21.11_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/401375354/systemd_229-4ubuntu21.11_armhf.deb -P systemd-tmp | wget http://launchpadlibrarian.net/401375353/systemd-sysv_229-4ubuntu21.11_armhf.deb -P systemd-tmp 
 sudo dpkg -i systemd-tmp/libpam-systemd_229-4ubuntu21.11_armhf.deb systemd-tmp/libsystemd0_229-4ubuntu21.11_armhf.deb systemd-tmp/systemd_229-4ubuntu21.11_armhf.deb systemd-tmp/systemd-sysv_229-4ubuntu21.11_armhf.deb
 rm systemd-tmp/libpam-systemd_229-4ubuntu21.11_armhf.deb | rm systemd-tmp/libsystemd0_229-4ubuntu21.11_armhf.deb | rm systemd-tmp/systemd_229-4ubuntu21.11_armhf.deb | rm systemd-tmp/systemd-sysv_229-4ubuntu21.11_armhf.deb
 rmdir systemd-tmp
-if [ ${1:-0} == "1" ]
+if [ ${1:-0} = "1" ]
 then
 sudo apt-mark hold libsystemd0 libpam-systemd systemd systemd-sysv
 fi
